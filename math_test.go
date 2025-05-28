48ec109a4d194bb3150977f4f32f79391b6af8ac
vinicius <vinc-analist@hotmail.com>
2025-05-28 19:11:32 -0300

    initial commit

---
+++ math_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	total := Soma(11, 15)
+	if total != 30 {
+		t.Errorf("Resultado %d da soma é invalido. O Resultado esperado é %d", total, 30)
+	}
+}
